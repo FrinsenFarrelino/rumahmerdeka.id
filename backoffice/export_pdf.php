30cbd7b1a50d39e891945427885d4a580a2c70f8
FrinsenFarrelino <frinsenfarrelino@gmail.com>
2025-08-04 16:00:08 +0700

    export pdf

--- backoffice/export_pdf.php
+++ backoffice/export_pdf.php
@@ -12,7 +12,7 @@ require_once 'api/get_participants.php';
 require_once '../vendor/tcpdf/tcpdf.php';
 
 // 3. Membuat Dokumen PDF
-$pdf = new TCPDF('L', PDF_UNIT, 'A4', true, 'UTF-8', false);
+$pdf = new TCPDF(PDF_PAGE_ORIENTATION, PDF_UNIT, PDF_PAGE_FORMAT, true, 'UTF-8', false);
 
 // Set informasi dokumen
 $pdf->SetCreator(PDF_CREATOR);
@@ -23,21 +23,21 @@ $pdf->SetSubject('Laporan Data Peserta');
 // Set header dan footer
 $pdf->setHeaderFont(Array(PDF_FONT_NAME_MAIN, '', PDF_FONT_SIZE_MAIN));
 $pdf->setFooterFont(Array(PDF_FONT_NAME_DATA, '', PDF_FONT_SIZE_DATA));
-$pdf->SetHeaderData('', 0, 'Laporan Data Peserta - Rumah Merdeka', 'Dibuat pada: ' . date('d-m-Y H:i'));
+$pdf->SetHeaderData('', 0, 'Laporan Data Peserta - Rumah Merdeka', 'Dibuat pada: ' . date('d-m-Y H:i') . ' oleh ' . $_SESSION['username']);
 
 // Set margin
-$pdf->SetMargins(10, 18, 10);
+$pdf->SetMargins(PDF_MARGIN_LEFT, PDF_MARGIN_TOP, PDF_MARGIN_RIGHT);
 $pdf->SetHeaderMargin(PDF_MARGIN_HEADER);
 $pdf->SetFooterMargin(PDF_MARGIN_FOOTER);
 
 // Set auto page breaks
-$pdf->SetAutoPageBreak(TRUE, 25);
+$pdf->SetAutoPageBreak(TRUE, PDF_MARGIN_BOTTOM);
 
 // Set font
-$pdf->SetFont('helvetica', '', 8);
+$pdf->SetFont('helvetica', '', 10);
 
 // 4. Menambahkan Halaman dan Konten
-$pdf->AddPage();
+$pdf->AddPage('L', 'A4'); // 'L' untuk Landscape
 
 $html = '<h2>Data Peserta Program Rumah Merdeka</h2>';
 $html .= '<table border="1" cellpadding="4" cellspacing="0">';
@@ -59,14 +59,15 @@ while ($row = $result->fetch_assoc()) {
     // Info Karyawan, Kontak, Status
     $html .= '<td style="vertical-align: top;"><b>' . htmlspecialchars($row['nama_karyawan']) . '</b><br><small>NIK: ' . htmlspecialchars($row['nik_karyawan']) . '</small><br><small>NIP: ' . htmlspecialchars($row['nomor_induk_karyawan']) . '</small></td>';
     $html .= '<td style="vertical-align: top;">' . htmlspecialchars($row['email_karyawan']) . '<br>' . htmlspecialchars($row['no_hp_karyawan']) . '<br><small>' . htmlspecialchars($row['alamat_karyawan']) . '</small></td>';
-    $html .= '<td style="vertical-align: top;">' . ucfirst($row['status_perkawinan']) . '<br><small>Penghasilan Sesuai: ' . ucfirst($row['penghasilan_sesuai']) . '</small></td>';
+    $html .= '<td style="vertical-align: top;">' . ucfirst($row['status_perkawinan']) . '<br><small>Gaji: ' . ucfirst($row['penghasilan_sesuai']) . '</small></td>';
     
     // Info Pasangan
     if ($row['status_perkawinan'] == 'menikah') {
         $html .= '<td style="vertical-align: top;"><b>' . htmlspecialchars($row['nama_pasangan']) . '</b><br><small>NIK: ' . htmlspecialchars($row['nik_pasangan']) . '</small></td>';
         $html .= '<td style="vertical-align: top;">' . htmlspecialchars($row['email_pasangan']) . '<br>' . htmlspecialchars($row['no_hp_pasangan']) . '<br><small>' . htmlspecialchars($row['alamat_pasangan']) . '</small></td>';
     } else {
-        $html .= '<td colspan="2" align="center" style="vertical-align: middle;">-</td>';
+        $html .= '<td align="center" style="vertical-align: middle;">-</td>';
+        $html .= '<td align="center" style="vertical-align: middle;">-</td>';
     }
 
     // Dokumen (Gambar KTP) - Menggunakan path absolut server untuk keandalan
@@ -104,9 +105,9 @@ $html .= '</tbody></table>';
 // Tulis HTML ke PDF
 $pdf->writeHTML($html, true, false, true, false, '');
 
+
 // 5. Menutup dan Mengirim PDF ke Browser
 $conn->close();
-$pdf->Output('data-peserta-rumah-merdeka.pdf', 'I');
-
+$pdf->Output('data-peserta-rumah-merdeka.pdf', 'I'); // 'I' untuk inline (tampil di browser), 'D' untuk download
 
 ?>
