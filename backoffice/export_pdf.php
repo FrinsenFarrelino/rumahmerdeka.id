8d44f90229513e88ca28ac471489f31f4920f69e
FrinsenFarrelino <frinsenfarrelino@gmail.com>
2025-08-04 15:41:27 +0700

    update path ktp and show preview

--- backoffice/export_pdf.php
+++ backoffice/export_pdf.php
@@ -12,7 +12,7 @@ require_once 'api/get_participants.php';
 require_once '../vendor/tcpdf/tcpdf.php';
 
 // 3. Membuat Dokumen PDF
-$pdf = new TCPDF(PDF_PAGE_ORIENTATION, PDF_UNIT, PDF_PAGE_FORMAT, true, 'UTF-8', false);
+$pdf = new TCPDF('L', PDF_UNIT, 'A4', true, 'UTF-8', false);
 
 // Set informasi dokumen
 $pdf->SetCreator(PDF_CREATOR);
@@ -26,60 +26,76 @@ $pdf->setFooterFont(Array(PDF_FONT_NAME_DATA, '', PDF_FONT_SIZE_DATA));
 $pdf->SetHeaderData('', 0, 'Laporan Data Peserta - Rumah Merdeka', 'Dibuat pada: ' . date('d-m-Y H:i'));
 
 // Set margin
-$pdf->SetMargins(PDF_MARGIN_LEFT, PDF_MARGIN_TOP, PDF_MARGIN_RIGHT);
+$pdf->SetMargins(10, 18, 10);
 $pdf->SetHeaderMargin(PDF_MARGIN_HEADER);
 $pdf->SetFooterMargin(PDF_MARGIN_FOOTER);
 
 // Set auto page breaks
-$pdf->SetAutoPageBreak(TRUE, PDF_MARGIN_BOTTOM);
+$pdf->SetAutoPageBreak(TRUE, 25);
 
 // Set font
-$pdf->SetFont('helvetica', '', 10);
+$pdf->SetFont('helvetica', '', 8);
 
 // 4. Menambahkan Halaman dan Konten
-$pdf->AddPage('L', 'A4'); // 'L' untuk Landscape
+$pdf->AddPage();
 
-$html = '<h1>Data Peserta Program Rumah Merdeka</h1>';
-$html .= '<table border="1" cellpadding="4">';
-// Header Tabel
-$html .= '<thead style="background-color:#eeeeee;">
+$html = '<h2>Data Peserta Program Rumah Merdeka</h2>';
+$html .= '<table border="1" cellpadding="4" cellspacing="0">';
+$html .= '<thead style="background-color:#f2f2f2; font-weight:bold;">
             <tr>
-                <th><b>Info Karyawan</b></th>
-                <th><b>Alamat & Kontak</b></th>
-                <th><b>Status</b></th>
-                <th><b>Info Pasangan</b></th>
-                <th><b>Alamat & Kontak Pasangan</b></th>
-                <th><b>Dokumen</b></th>
+                <th>Info Karyawan</th>
+                <th>Kontak & Alamat Karyawan</th>
+                <th>Status</th>
+                <th>Info Pasangan</th>
+                <th>Kontak & Alamat Pasangan</th>
+                <th>Dokumen KTP</th>
             </tr>
           </thead>';
 
 $html .= '<tbody>';
 // Body Tabel
 while ($row = $result->fetch_assoc()) {
-    $html .= '<tr>';
-    // Info Karyawan
-    $html .= '<td>' . htmlspecialchars($row['nama_karyawan']) . '<br><small>NIK: ' . htmlspecialchars($row['nik_karyawan']) . '</small><br><small>NIP: ' . htmlspecialchars($row['nomor_induk_karyawan']) . '</small></td>';
-    // Kontak & Alamat Karyawan
-    $html .= '<td>' . htmlspecialchars($row['email_karyawan']) . '<br>' . htmlspecialchars($row['no_hp_karyawan']) . '<br><small>' . htmlspecialchars($row['alamat_karyawan']) . '</small></td>';
-    // Status
-    $html .= '<td>' . ucfirst($row['status_perkawinan']) . '<br><small>Penghasilan Sesuai: ' . ucfirst($row['penghasilan_sesuai']) . '</small></td>';
+    $html .= '<tr nobr="true">';
+    // Info Karyawan, Kontak, Status
+    $html .= '<td style="vertical-align: top;"><b>' . htmlspecialchars($row['nama_karyawan']) . '</b><br><small>NIK: ' . htmlspecialchars($row['nik_karyawan']) . '</small><br><small>NIP: ' . htmlspecialchars($row['nomor_induk_karyawan']) . '</small></td>';
+    $html .= '<td style="vertical-align: top;">' . htmlspecialchars($row['email_karyawan']) . '<br>' . htmlspecialchars($row['no_hp_karyawan']) . '<br><small>' . htmlspecialchars($row['alamat_karyawan']) . '</small></td>';
+    $html .= '<td style="vertical-align: top;">' . ucfirst($row['status_perkawinan']) . '<br><small>Penghasilan Sesuai: ' . ucfirst($row['penghasilan_sesuai']) . '</small></td>';
     
     // Info Pasangan
     if ($row['status_perkawinan'] == 'menikah') {
-        $html .= '<td>' . htmlspecialchars($row['nama_pasangan']) . '<br><small>NIK: ' . htmlspecialchars($row['nik_pasangan']) . '</small></td>';
-        $html .= '<td>' . htmlspecialchars($row['email_pasangan']) . '<br>' . htmlspecialchars($row['no_hp_pasangan']) . '<br><small>' . htmlspecialchars($row['alamat_pasangan']) . '</small></td>';
+        $html .= '<td style="vertical-align: top;"><b>' . htmlspecialchars($row['nama_pasangan']) . '</b><br><small>NIK: ' . htmlspecialchars($row['nik_pasangan']) . '</small></td>';
+        $html .= '<td style="vertical-align: top;">' . htmlspecialchars($row['email_pasangan']) . '<br>' . htmlspecialchars($row['no_hp_pasangan']) . '<br><small>' . htmlspecialchars($row['alamat_pasangan']) . '</small></td>';
     } else {
-        $html .= '<td colspan="2" align="center">-</td>';
+        $html .= '<td colspan="2" align="center" style="vertical-align: middle;">-</td>';
     }
 
-    // Dokumen
-    $ktp_karyawan_url = 'http://' . $_SERVER['HTTP_HOST'] . dirname($_SERVER['PHP_SELF']) . '/../' . $row['path_ktp_karyawan'];
-    $dokumen_html = '<a href="' . $ktp_karyawan_url . '">KTP Karyawan</a>';
-    if ($row['status_perkawinan'] == 'menikah' && !empty($row['path_ktp_pasangan'])) {
-        $ktp_pasangan_url = 'http://' . $_SERVER['HTTP_HOST'] . dirname($_SERVER['PHP_SELF']) . '/../' . $row['path_ktp_pasangan'];
-        $dokumen_html .= '<br><a href="' . $ktp_pasangan_url . '">KTP Pasangan</a>';
+    // Dokumen (Gambar KTP) - Menggunakan path absolut server untuk keandalan
+    $dokumen_html = '';
+    // Mendapatkan direktori root proyek (satu level di atas 'backoffice')
+    $project_server_root = dirname(__DIR__); 
+    
+    // Proses KTP Karyawan
+    $ktp_karyawan_path_db = $row['path_ktp_karyawan'];
+    $full_path_karyawan = $project_server_root . '/' . ltrim($ktp_karyawan_path_db, '/');
+    if (!empty($ktp_karyawan_path_db) && file_exists($full_path_karyawan) && is_readable($full_path_karyawan)) {
+        $imageData = base64_encode(file_get_contents($full_path_karyawan));
+        $dokumen_html .= '<img src="@' . $imageData . '" width="120"><br/><small>KTP Karyawan</small>';
+    } else {
+        $dokumen_html .= '<small>KTP Karyawan tidak ada</small>';
     }
-    $html .= '<td>' . $dokumen_html . '</td>';
+    
+    // Proses KTP Pasangan
+    if ($row['status_perkawinan'] == 'menikah') {
+        $ktp_pasangan_path_db = $row['path_ktp_pasangan'];
+        $full_path_pasangan = $project_server_root . '/' . ltrim($ktp_pasangan_path_db, '/');
+        if (!empty($ktp_pasangan_path_db) && file_exists($full_path_pasangan) && is_readable($full_path_pasangan)) {
+            $imageData = base64_encode(file_get_contents($full_path_pasangan));
+            $dokumen_html .= '<br/><br/><img src="@' . $imageData . '" width="120"><br/><small>KTP Pasangan</small>';
+        } else {
+            $dokumen_html .= '<br/><br/><small>KTP Pasangan tidak ada</small>';
+        }
+    }
+    $html .= '<td style="vertical-align: top; text-align: center;">' . $dokumen_html . '</td>';
     
     $html .= '</tr>';
 }
@@ -88,9 +104,9 @@ $html .= '</tbody></table>';
 // Tulis HTML ke PDF
 $pdf->writeHTML($html, true, false, true, false, '');
 
-
 // 5. Menutup dan Mengirim PDF ke Browser
 $conn->close();
-$pdf->Output('data-peserta-rumah-merdeka.pdf', 'I'); // 'I' untuk inline (tampil di browser), 'D' untuk download
+$pdf->Output('data-peserta-rumah-merdeka.pdf', 'I');
+
 
 ?>
--- backoffice/participants.php
+++ backoffice/participants.php
@@ -42,37 +42,66 @@ require_once 'api/get_participants.php';
             <?php if ($result && $result->num_rows > 0): ?>
                 <?php while($row = $result->fetch_assoc()): ?>
                     <tr class="hover:bg-gray-50">
-                        <td class="px-5 py-4 border-b border-gray-200 bg-white text-sm">
+                        <td class="px-5 py-4 border-b border-gray-200 bg-white text-sm align-top">
                             <p class="text-gray-900 font-bold whitespace-no-wrap"><?php echo htmlspecialchars($row['nama_karyawan']); ?></p>
                             <p class="text-gray-600 whitespace-no-wrap">NIK: <?php echo htmlspecialchars($row['nik_karyawan']); ?></p>
                             <p class="text-gray-600 whitespace-no-wrap">NIP: <?php echo htmlspecialchars($row['nomor_induk_karyawan']); ?></p>
                         </td>
-                        <td class="px-5 py-4 border-b border-gray-200 bg-white text-sm">
+                        <td class="px-5 py-4 border-b border-gray-200 bg-white text-sm align-top">
                             <p class="text-gray-900 whitespace-no-wrap"><?php echo htmlspecialchars($row['email_karyawan']); ?></p>
                             <p class="text-gray-600 whitespace-no-wrap"><?php echo htmlspecialchars($row['no_hp_karyawan']); ?></p>
                             <p class="text-gray-500 whitespace-no-wrap mt-1 text-xs"><?php echo htmlspecialchars($row['alamat_karyawan']); ?></p>
                         </td>
-                        <td class="px-5 py-4 border-b border-gray-200 bg-white text-sm">
+                        <td class="px-5 py-4 border-b border-gray-200 bg-white text-sm align-top">
                              <span class="relative inline-block px-3 py-1 font-semibold <?php echo ($row['status_perkawinan'] == 'menikah') ? 'text-green-900' : 'text-yellow-900'; ?> leading-tight">
                                 <span aria-hidden class="absolute inset-0 <?php echo ($row['status_perkawinan'] == 'menikah') ? 'bg-green-200' : 'bg-yellow-200'; ?> opacity-50 rounded-full"></span>
                                 <span class="relative capitalize"><?php echo htmlspecialchars($row['status_perkawinan']); ?></span>
                             </span>
                             <p class="text-gray-600 whitespace-no-wrap mt-2">Penghasilan Sesuai: <span class="font-semibold"><?php echo ucfirst($row['penghasilan_sesuai']); ?></span></p>
                         </td>
-                        <td class="px-5 py-4 border-b border-gray-200 bg-white text-sm">
+                        <td class="px-5 py-4 border-b border-gray-200 bg-white text-sm align-top">
                             <p class="text-gray-900 whitespace-no-wrap"><?php echo ($row['status_perkawinan'] == 'menikah') ? htmlspecialchars($row['nama_pasangan']) : '-'; ?></p>
                             <p class="text-gray-600 whitespace-no-wrap"><?php echo ($row['status_perkawinan'] == 'menikah') ? 'NIK: ' . htmlspecialchars($row['nik_pasangan']) : ''; ?></p>
                         </td>
-                         <td class="px-5 py-4 border-b border-gray-200 bg-white text-sm">
+                         <td class="px-5 py-4 border-b border-gray-200 bg-white text-sm align-top">
                             <p class="text-gray-900 whitespace-no-wrap"><?php echo ($row['status_perkawinan'] == 'menikah' && $row['email_pasangan']) ? htmlspecialchars($row['email_pasangan']) : '-'; ?></p>
                             <p class="text-gray-600 whitespace-no-wrap"><?php echo ($row['status_perkawinan'] == 'menikah' && $row['no_hp_pasangan']) ? htmlspecialchars($row['no_hp_pasangan']) : ''; ?></p>
                             <p class="text-gray-500 whitespace-no-wrap mt-1 text-xs"><?php echo ($row['status_perkawinan'] == 'menikah' && $row['alamat_pasangan']) ? htmlspecialchars($row['alamat_pasangan']) : ''; ?></p>
                         </td>
-                        <td class="px-5 py-4 border-b border-gray-200 bg-white text-sm text-center">
-                            <a href="../<?php echo htmlspecialchars($row['path_ktp_karyawan']); ?>" target="_blank" class="text-blue-600 hover:text-blue-800 underline">KTP Karyawan</a>
-                            <?php if ($row['status_perkawinan'] == 'menikah' && !empty($row['path_ktp_pasangan'])): ?>
-                                <br>
-                                <a href="../<?php echo htmlspecialchars($row['path_ktp_pasangan']); ?>" target="_blank" class="text-blue-600 hover:text-blue-800 underline">KTP Pasangan</a>
+                        <td class="px-5 py-4 border-b border-gray-200 bg-white text-sm text-center align-top">
+                            <?php 
+                            // --- PERBAIKAN ---
+                            // Mendefinisikan direktori root proyek di dalam loop untuk memastikan variabel selalu ada.
+                            $project_server_root = dirname(__DIR__);
+
+                            // Jalur untuk web (URL di browser)
+                            $ktp_karyawan_web_path = '../' . $row['path_ktp_karyawan'];
+                            // Jalur untuk server (untuk file_exists)
+                            $ktp_karyawan_server_path = $project_server_root . '/' . ltrim($row['path_ktp_karyawan'], '/');
+
+                            if (!empty($row['path_ktp_karyawan']) && file_exists($ktp_karyawan_server_path)): ?>
+                                <a href="<?php echo htmlspecialchars($ktp_karyawan_web_path); ?>" target="_blank">
+                                    <img src="<?php echo htmlspecialchars($ktp_karyawan_web_path); ?>" alt="KTP Karyawan" class="mx-auto w-40 h-auto rounded-md shadow-sm hover:shadow-lg transition-shadow duration-300">
+                                </a>
+                                <p class="text-xs text-gray-500 mt-1">KTP Karyawan</p>
+                            <?php else: ?>
+                                <span class="text-xs text-gray-400">KTP Karyawan tidak tersedia</span>
+                            <?php endif; ?>
+
+                            <?php if ($row['status_perkawinan'] == 'menikah'): 
+                                // Jalur untuk web (URL di browser)
+                                $ktp_pasangan_web_path = '../' . $row['path_ktp_pasangan'];
+                                // Jalur untuk server (untuk file_exists)
+                                $ktp_pasangan_server_path = $project_server_root . '/' . ltrim($row['path_ktp_pasangan'], '/');
+
+                                if (!empty($row['path_ktp_pasangan']) && file_exists($ktp_pasangan_server_path)): ?>
+                                    <a href="<?php echo htmlspecialchars($ktp_pasangan_web_path); ?>" target="_blank" class="mt-4 inline-block">
+                                        <img src="<?php echo htmlspecialchars($ktp_pasangan_web_path); ?>" alt="KTP Pasangan" class="mx-auto w-40 h-auto rounded-md shadow-sm hover:shadow-lg transition-shadow duration-300">
+                                    </a>
+                                    <p class="text-xs text-gray-500 mt-1">KTP Pasangan</p>
+                                <?php else: ?>
+                                    <span class="text-xs text-gray-400 mt-4 inline-block">KTP Pasangan tidak tersedia</span>
+                                <?php endif; ?>
                             <?php endif; ?>
                         </td>
                     </tr>
